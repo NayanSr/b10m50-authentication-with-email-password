d177a996f471274e2abd2db52c49eb50b848810c
Nayan Suter <nayankbps@gmail.com>
2024-11-15 19:21:55 +0600

    Toggle password hidden or show. Accept terms and condition using checkbox.

--- src/assets/components/Register.jsx
+++ src/assets/components/Register.jsx
@@ -1,42 +1,49 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { createUserWithEmailAndPassword } from "firebase/auth";
-import auth from '../../firebase.init';
-
+import auth from "../../firebase.init";
+import { IoEyeOutline } from "react-icons/io5";
+import { FiEyeOff } from "react-icons/fi";
 
 const Register = () => {
-  const [errorMessage,setErrormessage]= useState('');
-  const [success, setSuccess]= useState(false);
-    
-const handleRegister= event =>{
+  const [errorMessage, setErrormessage] = useState("");
+  const [success, setSuccess] = useState(false);
+  const [show, setShow] = useState(false);
+
+  const handleRegister = (event) => {
     event.preventDefault();
-    setErrormessage('');
+    setErrormessage("");
     setSuccess(false);
     const email = event.target.email.value;
-    const password  = event.target.password.value;
+    const password = event.target.password.value;
+    const checkBox= event.target.checkBox.checked;
     const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
-    if(!passwordRegex.test(password)){
-      setErrormessage('Not strong password');
+    console.log(checkBox);
+    if(!checkBox){
+      setErrormessage('Please accept terms and conditions');
       return;
     }
-    if(password.length <6){
-      setErrormessage('Password should be 6 charecter at least');
-      return
+    if (!passwordRegex.test(password)) {
+      setErrormessage("Not strong password");
+      return;
     }
-   createUserWithEmailAndPassword(auth, email, password)
-   .then((result)=>{
-    console.log(result.user);
-    setSuccess(true)
-   })
-   .catch(error=>{
-    console.log(error);
-    setErrormessage(error.message);
-  
-   })
-}
+    if (password.length < 6) {
+      setErrormessage("Password should be 6 charecter at least");
+      return;
+    }
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        console.log(result.user);
+        setSuccess(true);
+      })
+      .catch((error) => {
+        console.log(error);
+        setErrormessage(error.message);
+      });
+  };
 
-    return (
-        <div>
-      <form onSubmit={handleRegister} className="w-3/4 mx-auto mt-8" >
+  return (
+    <div>
+      <form onSubmit={handleRegister} className="w-3/4 mx-auto mt-8">
         <label className="input input-bordered flex items-center gap-2">
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -47,7 +54,13 @@ const handleRegister= event =>{
             <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
             <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
           </svg>
-          <input type="email" name='email' className="grow" placeholder="Email" required/>
+          <input
+            type="email"
+            name="email"
+            className="grow"
+            placeholder="Email"
+            required
+          />
         </label>
         <label className="input input-bordered flex items-center gap-2 my-4">
           <svg
@@ -62,15 +75,51 @@ const handleRegister= event =>{
               clipRule="evenodd"
             />
           </svg>
-          <input type="password" name='password' className="grow" placeholder='Password' />
+          <input
+            type={show ? "text" : "password"}
+            name="password"
+            className="grow"
+            placeholder="Password"
+          />
+          {!show ? (
+            <button
+              onClick={() => {
+                setShow(!show);
+              }}
+              className="btn btn-xs"
+            >
+              <IoEyeOutline />
+            </button>
+          ) : (
+            <button
+              onClick={() => {
+                setShow(!show);
+              }}
+              className="btn btn-xs"
+            >
+              <FiEyeOff />
+            </button>
+          )}
         </label>
+        <div className="form-control ">
+          <label className="label cursor-pointer justify-start">    
+            <input
+              type="checkbox"
+              name="checkBox"
+              className="checkbox checkbox-primary"
+            />
+            <span className="label-text ml-8">Accept our conditions</span>
+          </label>
+        </div>
         <button className="btn btn-primary w-1/4"> Register</button>
 
-        {errorMessage && <h4 className='text-orange-900'>{errorMessage}</h4>}
-        {success && <h4 className='text-teal-600'>Successfully created user</h4>}
+        {errorMessage && <h4 className="text-orange-900">{errorMessage}</h4>}
+        {success && (
+          <h4 className="text-teal-600">Successfully created user</h4>
+        )}
       </form>
     </div>
-    );
+  );
 };
 
 export default Register;

9006ee354e2d2c70ae4a201be6ce09814765468c
Nayan Suter <nayankbps@gmail.com>
2024-11-15 18:45:37 +0600

    show message after user created or error & use regular expression

--- src/assets/components/Register.jsx
+++ src/assets/components/Register.jsx
@@ -1,20 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import auth from '../../firebase.init';
 
 
 const Register = () => {
+  const [errorMessage,setErrormessage]= useState('');
+  const [success, setSuccess]= useState(false);
     
 const handleRegister= event =>{
     event.preventDefault();
+    setErrormessage('');
+    setSuccess(false);
     const email = event.target.email.value;
     const password  = event.target.password.value;
+    const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+    if(!passwordRegex.test(password)){
+      setErrormessage('Not strong password');
+      return;
+    }
+    if(password.length <6){
+      setErrormessage('Password should be 6 charecter at least');
+      return
+    }
    createUserWithEmailAndPassword(auth, email, password)
    .then((result)=>{
     console.log(result.user);
+    setSuccess(true)
    })
    .catch(error=>{
     console.log(error);
+    setErrormessage(error.message);
+  
    })
 }
 
@@ -31,7 +47,7 @@ const handleRegister= event =>{
             <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
             <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
           </svg>
-          <input type="email" name='email' className="grow" placeholder="Email" />
+          <input type="email" name='email' className="grow" placeholder="Email" required/>
         </label>
         <label className="input input-bordered flex items-center gap-2 my-4">
           <svg
@@ -48,7 +64,10 @@ const handleRegister= event =>{
           </svg>
           <input type="password" name='password' className="grow" placeholder='Password' />
         </label>
-        <button className="btn btn-primary w-1/4"> Login</button>
+        <button className="btn btn-primary w-1/4"> Register</button>
+
+        {errorMessage && <h4 className='text-orange-900'>{errorMessage}</h4>}
+        {success && <h4 className='text-teal-600'>Successfully created user</h4>}
       </form>
     </div>
     );
